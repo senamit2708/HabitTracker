8d460be88fb4d1f108f9db23b158a700946d7b55
senamit2708 <senamit2708@gmail.com>
2017-11-05 09:48:38 +0530

    For udacity submission

--- app/src/main/java/com/example/senamit/habittracker/MainActivity.java
+++ app/src/main/java/com/example/senamit/habittracker/MainActivity.java
@@ -1,21 +1,25 @@
 package com.example.senamit.habittracker;
 
-import android.content.Intent;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.senamit.habittracker.data.HabitTrackerDbHelper;
 import com.example.senamit.habittracker.data.HabitContract.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    HabitTrackerDbHelper habitTrackerDbHelper;
-    SQLiteDatabase db;
+    private HabitTrackerDbHelper habitTrackerDbHelper;
+    private SQLiteDatabase db;
+    private EditText edtHabitName;
+    private EditText edtDays;
 
 
     @Override
@@ -23,25 +27,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtHabitName = (EditText) findViewById(R.id.edt_habit_name);
+        edtDays = (EditText) findViewById(R.id.edt_days);
+        Button btnSubmit = (Button) findViewById(R.id.btn_submit);
 
-        FloatingActionButton fabButton = (FloatingActionButton) findViewById(R.id.fab_button);
-
-        fabButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(MainActivity.this, CreateHabit.class);
-                startActivity(intent);
-            }
-        });
         habitTrackerDbHelper = new HabitTrackerDbHelper(MainActivity.this);
 
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertData();
+                displayDatabaseInfo();
+                edtHabitName.setText("");
+                edtDays.setText("");
+            }
 
+            private void insertData() {
+                db = habitTrackerDbHelper.getWritableDatabase();
+                ContentValues contentValues = new ContentValues();
+                String habitName = edtHabitName.getText().toString().trim();
+                String habitDays = edtDays.getText().toString().trim();
+                contentValues.put(Habitentry.COLUMN_HABIT_NAME, habitName);
+                contentValues.put(Habitentry.COLUMN_NUMBER_OF_DAYS, habitDays);
+                long RowId = db.insert(Habitentry.TABLE_NAME, null, contentValues);
 
+                if (RowId == -1) {
+                    Toast.makeText(MainActivity.this, "unsuccessful", Toast.LENGTH_SHORT).show();
 
+                } else {
+                    Toast.makeText(MainActivity.this, "successful", Toast.LENGTH_SHORT).show();
 
-
-
+                }
+            }
+        });
     }
 
     @Override
@@ -51,11 +69,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayDatabaseInfo() {
-
         db = habitTrackerDbHelper.getReadableDatabase();
-
-        String[] projection = {Habitentry._ID,Habitentry.COLUMN_HABIT_NAME, Habitentry.COLUMN_NUMBER_OF_DAYS};
-
+        String[] projection = {Habitentry._ID, Habitentry.COLUMN_HABIT_NAME, Habitentry.COLUMN_NUMBER_OF_DAYS};
         Cursor cursor = db.query(
                 Habitentry.TABLE_NAME,
                 projection,
@@ -65,33 +80,23 @@ public class MainActivity extends AppCompatActivity {
                 null,
                 null);
 
+        TextView displayHabit = (TextView) findViewById(R.id.display_habit);
+        try {
+            displayHabit.setText("the number of rows is " + cursor.getCount());
 
-        TextView displayHabit = (TextView)findViewById(R.id.display_habit);
+            int idColumnIndex = cursor.getColumnIndex(Habitentry._ID);
+            int habitNameColumnIndex = cursor.getColumnIndex(Habitentry.COLUMN_HABIT_NAME);
+            int numberOfDaysColumnIndex = cursor.getColumnIndex(Habitentry.COLUMN_NUMBER_OF_DAYS);
+            while (cursor.moveToNext()) {
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentHabitName = cursor.getString(habitNameColumnIndex);
+                String currentNumberOfDays = cursor.getString(numberOfDaysColumnIndex);
 
-        try{
-
-
-        displayHabit.setText("the number of rows is "+ cursor.getCount());
-
-//        displayHabit.append(Habitentry._ID+" - "+
-//            Habitentry.COLUMN_HABIT_NAME+" - "+
-//            Habitentry.COLUMN_NUMBER_OF_DAYS + "\n");
-
-        int idColumnIndex = cursor.getColumnIndex(Habitentry._ID);
-        int habitNameColumnIndex = cursor.getColumnIndex(Habitentry.COLUMN_HABIT_NAME);
-        int numberOfDaysColumnIndex = cursor.getColumnIndex(Habitentry.COLUMN_NUMBER_OF_DAYS);
-
-
-        while (cursor.moveToNext()) {
-            int currentID = cursor.getInt(idColumnIndex);
-            String currentHabitName = cursor.getString(habitNameColumnIndex);
-            String currentNumberOfDays = cursor.getString(numberOfDaysColumnIndex);
-
-            displayHabit.append(("\n" + currentID + " - " +
-                    currentHabitName + " - " +
-                    currentNumberOfDays));
-        }
-        }finally{
+                displayHabit.append(("\n" + currentID + " - " +
+                        currentHabitName + " - " +
+                        currentNumberOfDays));
+            }
+        } finally {
             cursor.close();
         }
     }
--- app/src/main/java/com/example/senamit/habittracker/data/HabitContract.java
+++ app/src/main/java/com/example/senamit/habittracker/data/HabitContract.java
@@ -2,24 +2,15 @@ package com.example.senamit.habittracker.data;
 
 import android.provider.BaseColumns;
 
-/**
- * Created by senamit on 4/11/17.
- */
 
 public class HabitContract {
 
-    //its private so that no one can use it outside
     private HabitContract() {
     }
 
-
-    //inner class for each table... that defines the table contents
     public static class Habitentry implements BaseColumns {
-
         public static final String TABLE_NAME = "Entry";
         public static final String COLUMN_HABIT_NAME = "HabitName";
         public static final String COLUMN_NUMBER_OF_DAYS = "NoOfDays";
-
-
     }
 }
